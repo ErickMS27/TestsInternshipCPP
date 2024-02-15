810cc57bda4962371b7851f99c56d810e5dc45d7
ErickMS27 <erickbackend@gmail.com>
2024-02-15 01:16:17 -0300

    Resolução da Quest 1.0) em Cpp

---
+++ Cpp/CalcMedia.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+int main() {
+    while (true) {
+        double nota_1, nota_2, nota_3, nota_4;
+
+        std::cout << "Digite a sua primeira nota: ";
+        std::cin >> nota_1;
+
+        while (nota_1 < 0 || nota_1 > 10) {
+            std::cout << "Nota inválida. Por favor, digite uma nota entre 0 a 10: ";
+            std::cin >> nota_1;
+        }
+
+        std::cout << "\nDigite a sua segunda nota: ";
+        std::cin >> nota_2;
+
+        while (nota_2 < 0 || nota_2 > 10) {
+            std::cout << "Nota inválida. Por favor, digite uma nota entre 0 a 10: ";
+            std::cin >> nota_2;
+        }
+
+        std::cout << "\nDigite a sua terceira nota: ";
+        std::cin >> nota_3;
+
+        while (nota_3 < 0 || nota_3 > 10) {
+            std::cout << "Nota inválida. Por favor, digite uma nota entre 0 a 10: ";
+            std::cin >> nota_3;
+        }
+
+        std::cout << "\nDigite a sua quarta nota: ";
+        std::cin >> nota_4;
+
+        while (nota_4 < 0 || nota_4 > 10) {
+            std::cout << "Nota inválida. Por favor, digite uma quarta entre 0 a 10: ";
+            std::cin >> nota_4;
+        }
+
+        double media = (nota_1 + nota_2 + nota_3 + nota_4) / 4;
+
+        std::cout << "\nNotas inseridas:\n";
+        std::cout << "Primeira nota: " << nota_1 << "\n";
+        std::cout << "Segunda nota: " << nota_2 << "\n";
+        std::cout << "Terceira nota: " << nota_3 << "\n";
+        std::cout << "Quarta nota: " << nota_4 << "\n";
+
+        std::cout << "\nMédia das notas = " << media << "\n";
+
+        std::string reiniciar;
+        std::cout << "\nDeseja reiniciar a aplicação? (Y/N): ";
+        std::cin >> reiniciar;
+
+        if (reiniciar != "y" && reiniciar != "Y") {
+            std::cout << "Aplicação encerrada.\n";
+            break;
+        }
+    }
+
+    return 0;
+}
